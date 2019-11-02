10d145b2c0c1f55f8919ff3e4afcf54d47f71adb
Naveen <naveenou@gmail.com>
2019-11-02 15:29:11 +0530

    creating component from cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
