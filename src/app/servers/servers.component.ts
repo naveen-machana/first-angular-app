5d4b32c217d1e7160a36a524587dfb24b24ed268
Naveen <naveenou@gmail.com>
2019-11-05 21:35:06 +0530

    ngIf and ng-template for else

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -18,6 +18,7 @@ export class ServersComponent implements OnInit {
   addNewServer = false;
   serverCreationStatus = 'No server is created';
   serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -28,6 +29,7 @@ export class ServersComponent implements OnInit {
   onAddServer() {
     this.serverCreationStatus =
       'Server was created! and server name is: ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServer(event: Event) {
